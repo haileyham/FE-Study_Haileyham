290dfec23d783fd134bc1605b1fdf0c1b1175096
hailey ham <roserealslow@gmail.com>
2023-09-09 13:30:59 +0900

    list 기본

--- infinite-scroller/src/pages/list/List.tsx
+++ infinite-scroller/src/pages/list/List.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useInView } from 'react-intersection-observer';
 
-export default function List() {
-  return (
-    <div>List</div>
+export default function List():JSX.Element {
+    const [ref, inView] = useInView();
+    const [page, setPage] = useState(0);
+    const [products, setProducts] = useState<string[]>([]);
+
+    const repeat = () => {
+        const repeatHello :string[] = Array(300).fill('hello');
+        setProducts(repeatHello);
+    }
+
+    return (
+        <>
+            <button onClick={repeat}>click</button>
+            {products?.map((a) => { return <div>{a}</div>})}
+            <div>List</div>
+        </>
   )
 }
