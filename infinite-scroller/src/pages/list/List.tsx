196916721e9686cdff40acbf7feaff446495d4c6
hailey ham <roserealslow@gmail.com>
2023-09-10 17:34:56 +0900

    useState타입 / list map

--- infinite-scroller/src/pages/list/List.tsx
+++ infinite-scroller/src/pages/list/List.tsx
@@ -5,7 +5,11 @@ export default function List():JSX.Element {
     const [ref, inView] = useInView();
     const [page, setPage] = useState(0);
     const [products, setProducts] = useState<string[]>([]);
-    const [item, setItem] = useState<string>(); // 초기값을 null로 설정하고 객체 또는 null을 허용
+    const [item, setItem] = useState<ASorN[]>([]); // 초기값을 null로 설정하고 객체 또는 null을 허용
+
+    interface ASorN{
+        [key: string]: string|number
+    }
 
 
     const repeat = () => {
@@ -15,13 +19,13 @@ export default function List():JSX.Element {
 
     const apiget = async () => {
         try {
-          const response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=10&_limit=10');
+          const response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10');
           if (!response.ok) {
             throw new Error('네트워크 오류'); // 오류 처리 예제
           }
           const json = await response.json();
           console.log(json);
-          setItem(json.title);
+          setItem(json);
         } catch (error) {
           console.error('에러 발생:', error);
         }
@@ -29,10 +33,15 @@ export default function List():JSX.Element {
 
     return (
         <>
-            <button onClick={repeat}>click</button>
+            {/* <button onClick={repeat}>click</button> */}
             <button onClick={apiget}>click</button>
-            {products?.map((a) => { return <div>{a}</div>})}
-        {item}
+            {/* {products?.map((a) => { return <div>{a}</div>})} */}
+            {item?.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+      ))}
             <div>List</div>
         </>
 )
