b164152cde9fb5ef0210b708ce585a0de6ea902e
hailey ham <roserealslow@gmail.com>
2023-09-17 06:14:08 +0900

    서울문화 api요청 / xml 파싱 및 config.json으로 apikey빼기

--- searchable/src/components/search/search.tsx
+++ searchable/src/components/search/search.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import config from '../../api/config.json';
 
-export default function search() {
+export default function Search(): JSX.Element {
+    const get = async (): Promise<void> => {
+        try {
+            // const apiKey = process.env.REACT_APP_API_KEY;
+            // console.log(apiKey);
+            const apiKey2 = config.api_key;
+            const response = await fetch(
+                `http://openapi.seoul.go.kr:8088/${apiKey2}/xml/culturalEventInfo/1/5/`,
+            );
+            if (!response.ok) {
+                throw new Error('오류');
+            }
+            const xmlText = await response.text();
+            const parser = new DOMParser();
+            let data = parser.parseFromString(xmlText, 'text/xml');
+            console.log(data);
+            const title = data.querySelector('TITLE');
+            console.log(title);
+        } catch (error) {
+            console.log('에러발생:', error);
+        }
+    };
+
+    useEffect(() => {
+        get();
+    }, []);
+
     return (
         <>
             <h1>찾아보자!</h1>
