23c3c403d5637c9a362ce702c458370b3c07e38c
hailey ham <roserealslow@gmail.com>
2023-09-16 14:08:01 +0900

    컴포넌트

---
+++ searchable/src/components/search/search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function search() {
+    return (
+        <>
+            <h1>찾아보자!</h1>
+            <input type="text" />
+            <button>검색</button>
+        </>
+    );
+}
--- searchable/src/pages/main/Main.tsx
+++ searchable/src/pages/main/Main.tsx
@@ -1,5 +1,10 @@
 import '../../styles/main.scss';
+import Search from '../../components/search/search';
 
 export default function Main() {
-    return <div>Main</div>;
+    return (
+        <>
+            <Search />;
+        </>
+    );
 }
