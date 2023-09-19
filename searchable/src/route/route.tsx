8786e61a4539075bf78206ed1626efcfc626c8c3
hailey ham <roserealslow@gmail.com>
2023-09-19 11:55:25 +0900

    경로추가

--- searchable/src/route/route.tsx
+++ searchable/src/route/route.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/main/Main';
-
-
+import Searching from '../pages/search/Search';
 
 export default function route() {
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Main/>} />
-    </Routes>
-</BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/search" element={<Searching />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
