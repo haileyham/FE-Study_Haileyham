94782aaead26e9c2ffc317d6bc51d204051870a0
hailey ham <roserealslow@gmail.com>
2023-09-19 11:55:16 +0900

    경로추가

--- searchable/src/pages/main/Main.tsx
+++ searchable/src/pages/main/Main.tsx
@@ -1,10 +1,22 @@
 import '../../styles/main.scss';
 import Search from '../../components/search/search';
+import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
+    const navigate = useNavigate();
+
     return (
         <>
             <Search />;
+            <div
+                className="searchBox"
+                onClick={() => {
+                    navigate('/search');
+                }}
+            >
+                search
+            </div>
+            ;
         </>
     );
 }
