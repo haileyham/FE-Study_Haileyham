bd3395b4a1552d87103cce58c5738397bcb98667
hailey ham <roserealslow@gmail.com>
2023-09-20 15:12:32 +0900

    handleInputChange로 input받고 타입지정 / onChange 붙이기

--- searchable/src/pages/search/Search.tsx
+++ searchable/src/pages/search/Search.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import config from '../../api/config.json';
 import { v4 as uuidv4 } from 'uuid';
+import '../../styles/main.scss';
 
 // XML 파싱한 것 json으로 변환해서 사용하기
-export default function Searching() {
+export default function Searching(): JSX.Element {
     const [eventData, setEventData] = useState<CulturalEvent[]>([]);
     interface CulturalEvent {
         [key: string]: string | null | undefined;
     }
+
+    const [title, setTitle] = useState<string>('');
 
     const get = async (): Promise<void> => {
         try {
@@ -15,7 +18,9 @@ export default function Searching() {
             // console.log(apiKey);
             const apiKey2 = config.api_key;
             const response = await fetch(
-                `http://openapi.seoul.go.kr:8088/${apiKey2}/xml/culturalEventInfo/1/5/`,
+                `http://openapi.seoul.go.kr:8088/${apiKey2}/xml/culturalEventInfo/1/1000/ /${title}`, // 띄어쓰기 해야 TITLE 검색 가능 / 옵션1(분류) / 옵션2(공연/행사명) / 옵션3(날짜/시간 YYYY-MM-DD)
+                //한번에 최대 1000개까지 호출가능(총데이터수 약3600개)
+                //옛날것도 가져오는 것 같군 / 추후 시간 조정해야겠다
             );
             if (!response.ok) {
                 throw new Error('오류');
@@ -76,24 +81,38 @@ export default function Searching() {
         get();
     }, []);
 
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>,
+    ): void => {
+        setTitle(e.target.value);
+    };
+
     return (
         <>
-            <h1>찾아보자!</h1>
-            <input type="text" />
-            <button>검색</button>
-            {eventData &&
-                eventData.map((data) => {
-                    return (
-                        <div key={data.id}>
-                            <h2>{data.TITLE}</h2>
-                            <p>{data.DATE}</p>
-                            <p>{data.GUNAME}</p>
-                            <p>{data.IS_FREE}</p>
-                            <p>{data.PLACE}</p>
-                            <p>{data.CODENAME}</p>
-                        </div>
-                    );
-                })}
+            <div className="searchContainer">
+                <header className="searchHeader">
+                    <h1>찾아보자!</h1>
+                    <input onChange={handleInputChange} type="text" />
+                    <button>검색</button>
+                    <p>{title}</p>
+                </header>
+                <main className="cultureContainer">
+                    {eventData &&
+                        eventData.map((data) => {
+                            return (
+                                <div key={data.id} className="cultureBox">
+                                    <h2>{data.TITLE}</h2>
+                                    <img src={data.MAIN_IMG as string} alt="" />
+                                    <p>{data.DATE}</p>
+                                    <p>{data.GUNAME}</p>
+                                    <p>{data.IS_FREE}</p>
+                                    <p>{data.PLACE}</p>
+                                    <p>{data.CODENAME}</p>
+                                </div>
+                            );
+                        })}
+                </main>
+            </div>
         </>
     );
 }
