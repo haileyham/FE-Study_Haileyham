5d9ad703d6ca98a9cb15c1889d9a7fac1d3b2de8
hailey ham <roserealslow@gmail.com>
2023-09-21 13:32:50 +0900

    sessionStorage에서 데이터 먼저 가져오고 / 집어넣도록

--- searchable/src/pages/search/Search.tsx
+++ searchable/src/pages/search/Search.tsx
@@ -3,6 +3,7 @@ import config from '../../api/config.json';
 import { v4 as uuidv4 } from 'uuid';
 import '../../styles/main.scss';
 import useDebounce from '../../hooks/useDebounce';
+import { getSessionData, setSessionData } from '../../utils/sessionStorage';
 
 // XML 파싱한 것 json으로 변환해서 사용하기
 export default function Searching(): JSX.Element {
@@ -16,67 +17,86 @@ export default function Searching(): JSX.Element {
     const debounced = useDebounce(title, 700);
 
     const get = async (): Promise<void> => {
-        try {
-            // const apiKey = process.env.REACT_APP_API_KEY;
-            // console.log(apiKey);
-            const apiKey2 = config.api_key;
-            const response = await fetch(
-                `http://openapi.seoul.go.kr:8088/${apiKey2}/xml/culturalEventInfo/1/1000/ /${debounced}`, // 띄어쓰기 해야 TITLE 검색 가능 / 옵션1(분류) / 옵션2(공연/행사명) / 옵션3(날짜/시간 YYYY-MM-DD)
-                //한번에 최대 1000개까지 호출가능(총데이터수 약3600개)
-                //옛날것도 가져오는 것 같군 / 추후 시간 조정해야겠다
-            );
-            if (!response.ok) {
-                throw new Error('오류');
+        const existingData = getSessionData(debounced);
+        if (existingData) {
+            console.log(existingData);
+            setEventData(existingData);
+        } else {
+            try {
+                // const apiKey = process.env.REACT_APP_API_KEY;
+                // console.log(apiKey);
+                const apiKey2 = config.api_key;
+                const response = await fetch(
+                    `http://openapi.seoul.go.kr:8088/${apiKey2}/xml/culturalEventInfo/1/1000/ /${debounced}`, // 띄어쓰기 해야 TITLE 검색 가능 / 옵션1(분류) / 옵션2(공연/행사명) / 옵션3(날짜/시간 YYYY-MM-DD)
+                    //한번에 최대 1000개까지 호출가능(총데이터수 약3600개)
+                    //옛날것도 가져오는 것 같군 / 추후 시간 조정해야겠다
+                );
+                if (!response.ok) {
+                    throw new Error('오류');
+                }
+                const xmlText = await response.text();
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
+
+                const culturalEvents: CulturalEvent[] = [];
+
+                interface CulturalEvent {
+                    [key: string]: string | null | undefined;
+                }
+
+                const rowElements = xmlDoc.querySelectorAll('row');
+                // console.log(rowElements);
+                rowElements.forEach((rowElement) => {
+                    const culturalEvent = {
+                        //key값 주기위해서 id부여
+                        id: uuidv4(), // UUID를 사용한 고유한 식별자
+                        CODENAME:
+                            rowElement.querySelector('CODENAME')?.textContent,
+                        GUNAME: rowElement.querySelector('GUNAME')?.textContent,
+                        TITLE: rowElement.querySelector('TITLE')?.textContent,
+                        DATE: rowElement.querySelector('DATE')?.textContent,
+                        PLACE: rowElement.querySelector('PLACE')?.textContent,
+                        ORG_NAME:
+                            rowElement.querySelector('ORG_NAME')?.textContent,
+                        USE_TRGT:
+                            rowElement.querySelector('USE_TRGT')?.textContent,
+                        USE_FEE:
+                            rowElement.querySelector('USE_FEE')?.textContent,
+                        PLAYER: rowElement.querySelector('PLAYER')?.textContent,
+                        PROGRAM:
+                            rowElement.querySelector('PROGRAM')?.textContent,
+                        ETC_DESC:
+                            rowElement.querySelector('ETC_DESC')?.textContent,
+                        ORG_LINK:
+                            rowElement.querySelector('ORG_LINK')?.textContent,
+                        MAIN_IMG:
+                            rowElement.querySelector('MAIN_IMG')?.textContent,
+                        RGSTDATE:
+                            rowElement.querySelector('RGSTDATE')?.textContent,
+                        TICKET: rowElement.querySelector('TICKET')?.textContent,
+                        STRTDATE:
+                            rowElement.querySelector('STRTDATE')?.textContent,
+                        END_DATE:
+                            rowElement.querySelector('END_DATE')?.textContent,
+                        THEMECODE:
+                            rowElement.querySelector('THEMECODE')?.textContent,
+                        LOT: rowElement.querySelector('LOT')?.textContent,
+                        LAT: rowElement.querySelector('LAT')?.textContent,
+                        IS_FREE:
+                            rowElement.querySelector('IS_FREE')?.textContent,
+                        HMPG_ADDR:
+                            rowElement.querySelector('HMPG_ADDR')?.textContent,
+                    };
+
+                    culturalEvents.push(culturalEvent);
+                });
+                console.log(culturalEvents);
+                setEventData(culturalEvents);
+                // console.log(eventData);
+                setSessionData(debounced, culturalEvents);
+            } catch (error) {
+                console.log('에러발생:', error);
             }
-            const xmlText = await response.text();
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
-
-            const culturalEvents: CulturalEvent[] = [];
-
-            interface CulturalEvent {
-                [key: string]: string | null | undefined;
-            }
-
-            const rowElements = xmlDoc.querySelectorAll('row');
-            // console.log(rowElements);
-            rowElements.forEach((rowElement) => {
-                const culturalEvent = {
-                    //key값 주기위해서 id부여
-                    id: uuidv4(), // UUID를 사용한 고유한 식별자
-                    CODENAME: rowElement.querySelector('CODENAME')?.textContent,
-                    GUNAME: rowElement.querySelector('GUNAME')?.textContent,
-                    TITLE: rowElement.querySelector('TITLE')?.textContent,
-                    DATE: rowElement.querySelector('DATE')?.textContent,
-                    PLACE: rowElement.querySelector('PLACE')?.textContent,
-                    ORG_NAME: rowElement.querySelector('ORG_NAME')?.textContent,
-                    USE_TRGT: rowElement.querySelector('USE_TRGT')?.textContent,
-                    USE_FEE: rowElement.querySelector('USE_FEE')?.textContent,
-                    PLAYER: rowElement.querySelector('PLAYER')?.textContent,
-                    PROGRAM: rowElement.querySelector('PROGRAM')?.textContent,
-                    ETC_DESC: rowElement.querySelector('ETC_DESC')?.textContent,
-                    ORG_LINK: rowElement.querySelector('ORG_LINK')?.textContent,
-                    MAIN_IMG: rowElement.querySelector('MAIN_IMG')?.textContent,
-                    RGSTDATE: rowElement.querySelector('RGSTDATE')?.textContent,
-                    TICKET: rowElement.querySelector('TICKET')?.textContent,
-                    STRTDATE: rowElement.querySelector('STRTDATE')?.textContent,
-                    END_DATE: rowElement.querySelector('END_DATE')?.textContent,
-                    THEMECODE:
-                        rowElement.querySelector('THEMECODE')?.textContent,
-                    LOT: rowElement.querySelector('LOT')?.textContent,
-                    LAT: rowElement.querySelector('LAT')?.textContent,
-                    IS_FREE: rowElement.querySelector('IS_FREE')?.textContent,
-                    HMPG_ADDR:
-                        rowElement.querySelector('HMPG_ADDR')?.textContent,
-                };
-
-                culturalEvents.push(culturalEvent);
-            });
-            console.log(culturalEvents);
-            setEventData(culturalEvents);
-            // console.log(eventData);
-        } catch (error) {
-            console.log('에러발생:', error);
         }
     };
 
