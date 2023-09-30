795453fa3bd289935b43208aa0a594e5dd407f78
hailey ham <roserealslow@gmail.com>
2023-09-30 23:51:53 +0900

    delete

--- searchable/src/pages/search/Search.tsx
+++ searchable/src/pages/search/Search.tsx
@@ -141,17 +141,12 @@ export default function Searching(): JSX.Element {
             console.log('Tab');
         }
         if (e.key === 'ArrowDown') {
-            // console.log('아래');
             setSearchIndex((a) => (a !== eventDataReco.length ? a + 1 : 0));
         }
         if (e.key === 'ArrowUp') {
-            // console.log('위');
             setSearchIndex((a) => (a !== 0 ? a - 1 : eventDataReco.length));
         }
-        // console.log(e.code);
     };
-    // console.log(eventDataReco);
-    // console.log(eventData);
 
     return (
         <>
