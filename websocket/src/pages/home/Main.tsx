7f2446f299e2335b8774b83b59b95b7b50e403fe
hailey ham <roserealslow@gmail.com>
2023-10-03 12:37:06 +0900

    Chat페이지 navigate 연결

--- websocket/src/pages/home/Main.tsx
+++ websocket/src/pages/home/Main.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 export default function Main() {
-    return <div>Main</div>;
+    const navigate = useNavigate();
+    return (
+        <>
+            <ChatPageBtn
+                onClick={() => {
+                    navigate('/chat');
+                }}
+            >
+                채팅하기
+            </ChatPageBtn>
+        </>
+    );
 }
+
+const ChatPageBtn = styled.button`
+    width: 20rem;
+    padding: 1rem;
+    border-radius: 5px;
+    border: none;
+    font-size: 1.2rem;
+    font-weight: 900;
+    color: #a724fe;
+    background-color: #eedcfa;
+    cursor: pointer;
+    &:hover {
+        background-color: #c1b3ff;
+    }
+`;
