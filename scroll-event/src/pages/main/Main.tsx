f04565512d5cbb47da60d6071d5143e192a86e36
hailey ham <roserealslow@gmail.com>
2023-10-11 15:15:09 +0900

    sticky 사용하여 스크롤이벤트

--- scroll-event/src/pages/main/Main.tsx
+++ scroll-event/src/pages/main/Main.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/main.scss';
+import React, { useEffect } from 'react';
 
 export default function Main() {
-    return <div>Main</div>;
+    const navigate = useNavigate();
+
+    window.addEventListener('scroll', function () {
+        console.log(window.scrollY);
+    });
+
+    return (
+        <div style={{ height: '1000vh' }}>
+            <div className="box">
+                <img
+                    src={process.env.PUBLIC_URL + '/1.jpg'}
+                    className="image"
+                    alt="adventure"
+                />
+                <div style={{ clear: 'both' }}></div>
+                <div className="text">
+                    hellohellohellohellohellohe
+                    llohellohellohellohellohellohello hellohellohellohellohello
+                </div>
+                <div style={{ clear: 'both' }}></div>
+                <div className="text">
+                    hellohellohellohellohellohe
+                    llohellohellohellohellohellohello hellohellohellohellohello
+                </div>
+                <div style={{ clear: 'both' }}></div>
+                <div className="text">
+                    hellohellohellohellohellohe
+                    llohellohellohellohellohellohello hellohellohellohellohello
+                </div>
+                <div style={{ clear: 'both' }}></div>
+                <div className="text">
+                    hellohellohellohellohellohe
+                    llohellohellohellohellohellohello hellohellohellohellohello
+                </div>
+            </div>
+        </div>
+    );
 }
