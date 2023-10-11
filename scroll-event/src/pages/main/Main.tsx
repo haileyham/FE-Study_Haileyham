3aa75eaf30a4c6b00236e7f0f06d05db7ad33d10
hailey ham <roserealslow@gmail.com>
2023-10-11 15:36:35 +0900

    스크롤 이미지 sticky 변경

--- scroll-event/src/pages/main/Main.tsx
+++ scroll-event/src/pages/main/Main.tsx
@@ -1,19 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 import '../../styles/main.scss';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Main() {
     const navigate = useNavigate();
+    const [img, setImg] = useState(1);
 
     window.addEventListener('scroll', function () {
         console.log(window.scrollY);
     });
 
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition >= 1200) {
+            setImg(2);
+        } else {
+            setImg(1);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div style={{ height: '1000vh' }}>
             <div className="box">
                 <img
-                    src={process.env.PUBLIC_URL + '/1.jpg'}
+                    src={process.env.PUBLIC_URL + `/${img}.jpg`}
                     className="image"
                     alt="adventure"
                 />
